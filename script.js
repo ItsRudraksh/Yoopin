c3fbc14924a8d9f413563ba0d81b97dfc87fc050
ItsRudraksh <rudrakshkapoor2004@gmail.com>
2024-02-06 00:38:20 +0530

    Too many changes

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const imageList = document.getElementById("image-list");
+let currentIndex = 0;
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % imageList.children.length;
+  updateCarousel();
+}
+
+function prevImage() {
+  currentIndex =
+    (currentIndex - 1 + imageList.children.length) % imageList.children.length;
+  updateCarousel();
+}
+
+function updateCarousel() {
+  const translateValue = -currentIndex * 100 + "%";
+  imageList.style.transform = "translateX(" + translateValue + ")";
+}
+
+document.getElementById("left-arrow").addEventListener("click", prevImage);
+document.getElementById("right-arrow").addEventListener("click", nextImage);
 const playBtn = document.getElementById("play-btn");
 const video = document.getElementById("cycle-video");
 let playing = false;
@@ -21,35 +42,3 @@ playBtn.addEventListener("click", () => {
     `;
   }
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-  const spacer = document.querySelector(".spacer");
-
-  window.addEventListener("scroll", function () {
-    if (window.scrollY > spacer.offsetTop) {
-      window.scrollTo(0, spacer.offsetTop);
-    }
-  });
-});
-
-const imageList = document.getElementById("image-list");
-let currentIndex = 0;
-
-function nextImage() {
-  currentIndex = (currentIndex + 1) % imageList.children.length;
-  updateCarousel();
-}
-
-function prevImage() {
-  currentIndex =
-    (currentIndex - 1 + imageList.children.length) % imageList.children.length;
-  updateCarousel();
-}
-
-function updateCarousel() {
-  const translateValue = -currentIndex * 100 + "%";
-  imageList.style.transform = "translateX(" + translateValue + ")";
-}
-
-document.getElementById("left-arrow").addEventListener("click", prevImage);
-document.getElementById("right-arrow").addEventListener("click", nextImage);
